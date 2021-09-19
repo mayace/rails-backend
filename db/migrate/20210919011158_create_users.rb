f4dba7c4b05746b0a7e41233c1b3cc144b68dc07
ctejaxun <cesar.etx@gmail.com>
2021-09-18 19:26:11 -0600

    add-users - genera modelo user

---
+++ db/migrate/20210919011158_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :fullname
+      t.string :username
+      t.string :password_digest
+      t.string :email
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
